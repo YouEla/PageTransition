9f5e3630841387184ca3131c74b45def1dd109cb
youssef elaamraoui <youssef_2001@hotmail.it>
2019-12-04 19:22:10 +0100

    Cambiamento:non più presenti le immagini, inserimento del testo che si rivela all'animazione

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 let tempo=document.querySelector('#time');
-let c=0;
-let i=0;
 
 //Inizialise the timeline
 var tl = gsap.timeline();
 
 //The animations 
 window.onclick=function start_animation(){
+    document.querySelector('#time').style.zIndex='1';
     document.querySelector('#hide_block').style.zIndex='1';
+
 tl.from('#hide_block', {opacity:0,xPercent: -100});
 tl.to('#hide_block', {opacity:1,xPercent: 0, duration: .8,ease: 'expo.inOut'});
 tl.to('#hide_block', {opacity:1,xPercent: 100, duration: .8,ease: 'expo.inOut'});
-    setTimeout(a, 680);
+
+    setTimeout(a, 40);
 }
 
 function finish_animation(){
 tl.to('#hide_block', {opacity:1,xPercent: 200, duration: .8,ease: 'expo.inOut'});
 tl.to('#hide_block', {opacity:0,xPercent: 250, duration: .8,ease: 'expo.inOut'});
+
     document.querySelector('#hide_block').style.zIndex='0';
 }
 
 const a=async function date () {
-    document.querySelector('#contenuto').style.backgroundImage = "url('/img/levin-i8IPxSMJWtA-unsplash.jpg')";
-    document.querySelector('#time').style.zIndex='1';
+
+    document.querySelector('#contenuto').style.backgroundColor='#001f3f';
     document.querySelector('#time').style.color='white'; //I used it before because I have tried the site with a light image, and I had instead a black color for the font
+
     tl.from('#time', {fontSize: 30,opacity:1,y: 20,duration:.0});
-    tl.to('#time', {fontSize: 45,opacity:1,y: window.innerHeight/5, duration:.8,ease: 'expo.inOut'});
+    tl.to('#time', {fontSize: 45,opacity:1,y: 200, duration:.8,ease: 'expo.inOut'});
+    tl.to('#text_reveal', {opacity:1,y: 50, duration:.8,ease: 'expo.inOut'},'-=.4');
+    tl.to('#text_reveal', {opacity:0,y: 20, duration:.6,ease: 'expo.inOut'});
     tl.to('#time', {fontSize: 30,opacity:1,y: 20, duration: .9,ease: 'expo.inOut'});
     
     for (let i = 1961; i < 1989; i++) {
             await increment_number();// wait 100 ms, then print 
             tempo.innerHTML=i;
     } //if you want to take the screen size use window.screen.height for y and .width for x
-    document.querySelector('#contenuto').style.backgroundImage = "url('/img/nikita-karimov-lvJZhHOIJJ4-unsplash.jpg')";
+
+    document.querySelector('#contenuto').style.backgroundColor='rebeccapurple';
+ 
     finish_animation();
 };
 
